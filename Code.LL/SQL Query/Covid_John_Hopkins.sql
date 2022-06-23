d7023dce08b47fd6f0ab56c5b7e16e6d51805c21
lizzifer991 <elizabeth.loy88@gmail.com>
2022-06-22 21:17:33 -0500

    updating code adding SQL query

---
+++ Code.LL/SQL Query/Covid_John_Hopkins.sql
@@ -0,0 +1,7 @@
+CREATE TABLE case_state (
+	"state" VARCHAR NOT NULL,
+	"date" DATE NOT NULL,
+	"total_population" VARCHAR NOT NULL,
+	"cumulative_cases" VARCHAR NOT NULL,
+	"cumulative_deaths" VARCHAR NOT NULL
+);
